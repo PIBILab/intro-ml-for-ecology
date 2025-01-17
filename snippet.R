2bce1e50e20afee2a357b028b0ec785e1770f12a
PIBILab <ecotec.ufs@outlook.com>
2025-01-17 12:02:59 -0300

    test

---
+++ snippet.R
@@ -0,0 +1,13 @@
+
+snippet rscript
+# ${1:Description}
+
+# Author: ${2:Name}
+# Version: `r Sys.Date()`
+
+# Packages
+library(tidyverse)
+
+# ============================================================================
+
+${3:# Code}
